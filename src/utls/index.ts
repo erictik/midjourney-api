54880d25a6ecf5a39fea56e66546bc6f14c69fcb
Eric <eric@baochi.xin>
2023-05-28 00:47:37 +0800

    use snowyflake

--- src/utls/index.ts
+++ src/utls/index.ts
@@ -1,6 +1,4 @@
-import { Snowflake } from "./snowflake";
-
-export * from "./snowflake";
+import { Snowyflake, Epoch } from "snowyflake";
 
 export const sleep = async (ms: number): Promise<void> =>
   await new Promise((resolve) => setTimeout(resolve, ms));
@@ -8,5 +6,10 @@ export const sleep = async (ms: number): Promise<void> =>
 export const random = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min) + min);
 
-const snowflake = new Snowflake(0);
+// const snowflake = new Snowflake(1);
+const snowflake = new Snowyflake({
+  workerId: 1n,
+  epoch: Epoch.Twitter, // BigInt timestamp
+});
+
 export const nextNonce = (): string => snowflake.nextId().toString();
