35b7030d2896d0d72b87b685cebf696cc19d392f
Eric <eric@baochi.xin>
2023-06-12 19:53:14 +0800

    node-fetch 2.4

--- example/imagine.js
+++ example/imagine.js
@@ -14,7 +14,7 @@ async function main() {
     SalaiToken: process.env.SALAI_TOKEN,
     Debug: true,
     SessionId: process.env.SALAI_TOKEN || "8bb7f5b79c7a49f7d0824ab4b8773a81",
-  } );
+  });
   const msg = await client.Imagine("A little pink elephant", (uri) => {
     console.log("loading", uri);
   });
