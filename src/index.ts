aee5dc8ba6a516d6597d157a20bb0bfba82fc28d
Eric <eric@baochi.xin>
2023-06-18 11:06:47 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./midjourney.message";
 export * from "./ws.message";
 export * from "./interfaces/index";
 export * from "./midjourne.api";
+export * from "./command";
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
-import { DefaultMJConfig, Midjourney, WsMessage } from "../src";
-import { nextNonce } from "../src/utls";
+import { Command, DefaultMJConfig, Midjourney, WsMessage } from "../src";
 /**
  *
  * a simple example of how to use the imagine command
@@ -9,10 +8,17 @@ import { nextNonce } from "../src/utls";
  * ```
  */
 
-function test2() {
+async function test2() {
   const config = {
     ...DefaultMJConfig,
-    ...{},
+    ...{
+      ServerId: <string>process.env.SERVER_ID,
+      ChannelId: <string>process.env.CHANNEL_ID,
+      SalaiToken: <string>process.env.SALAI_TOKEN,
+    },
   };
+
+  const command = new Command(config);
+  const msg = await command.getCommand("imagine");
 }
 test2();
