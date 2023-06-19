4792cfb99a53af47c03e10992af5294f3fc12535
Eric <eric@baochi.xin>
2023-06-19 20:09:20 +0800

    rename blend

--- example/imagine-blend.ts
+++ example/imagine-blend.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { Midjourney } from "../src";
 /**
  *
- * a simple example of how to use the imagine command
+ * a simple example of how to use the blend
  * ```
- * npx tsx example/imagine-uri.ts
+ * npx tsx example/blend.ts
  * ```
  */
 async function main() {
@@ -14,8 +14,9 @@ async function main() {
     SalaiToken: <string>process.env.SALAI_TOKEN,
     Debug: true,
   });
+  await client.Connect();
   const msg = await client.Imagine(
-    "https://media.discordapp.net/attachments/1094892992281718894/1106660210380132503/Soga_A_Greek_man_with_mustache_in_national_costume_riding_a_don_3255e7c1-38ee-4892-b7c7-9f0dc3f2786d.png?width=1040&height=1040 https://cdn.discordapp.com/attachments/1094892992281718894/1106798152188702720/Soga__489d80b2-db74-4a93-a998-881a9542abbe.png  cool boy",
+    "https://media.discordapp.net/attachments/1094892992281718894/1106660210380132503/Soga_A_Greek_man_with_mustache_in_national_costume_riding_a_don_3255e7c1-38ee-4892-b7c7-9f0dc3f2786d.png?width=1040&height=1040 https://cdn.discordapp.com/attachments/1094892992281718894/1106798152188702720/Soga__489d80b2-db74-4a93-a998-881a9542abbe.png",
     (uri: string, progress: string) => {
       console.log("loading", uri, "progress", progress);
     }
