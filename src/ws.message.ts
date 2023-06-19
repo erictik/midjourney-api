1148d1de203717493e44095665cd44477b84c484
Eric <eric@baochi.xin>
2023-06-19 22:26:49 +0800

    fix

--- src/ws.message.ts
+++ src/ws.message.ts
@@ -159,7 +159,7 @@ export class WsMessage {
     }
   }
   private processingImage(message: any) {
-    const { content = "", id, attachments, flags } = message;
+    const { content, id, attachments, flags } = message;
     if (!content) {
       return;
     }
