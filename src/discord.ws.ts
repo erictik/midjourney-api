6020c454a4174e658f27e793149a87606a93c096
york-yu-ctw <yu.z@ctw.inc>
2025-05-02 17:21:02 +0900

    chore: fix problem of can not return finished image

--- src/discord.ws.ts
+++ src/discord.ws.ts
@@ -176,7 +176,7 @@ export class WsMessage {
       }
     }
 
-    if (!nonce && attachments?.length > 0 && components?.length > 0) {
+    if (nonce && attachments?.length > 0 && components?.length > 0) {
       this.done(message);
       return;
     }
